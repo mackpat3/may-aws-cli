b3d46aacbb70ea63ddb06d268e5c1000be94d150
mackpat3 <mackpat3@gmail.com>
2025-06-27 21:52:57 -0400

    change more

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-vpc_id=$(aws ec2 create-vpc --cidr-block 172.16.0.0/16 --tag-specification "ResourceType=vpc,Tags=[{Key=Name,Value=Kaizen}]" --query Vpc.VpcId --output text)
+vpc_cidr="192.168.0.0/16"
+subnet1_cidr="192.168.1.0/24"
+region="us-east-2"
 
-subnet1_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 172.16.1.0/24 --query Subnet.SubnetId --output text)
+vpc_id=$(aws ec2 create-vpc --cidr-block $vpc_cidr --region $region --tag-specification "ResourceType=vpc,Tags=[{Key=Name,Value=Kaizen}]" --query Vpc.VpcId --output text)
 
-igw_id=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
+subnet1_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet1_cidr --region $region --query Subnet.SubnetId --output text)
 
-aws ec2 attach-internet-gateway --vpc-id $vpc_id --internet-gateway-id $igw_id
+igw_id=$(aws ec2 create-internet-gateway --region $region --query InternetGateway.InternetGatewayId --output text)
 
-rt_id=$(aws ec2 create-route-table --vpc-id $vpc_id --query RouteTable.RouteTableId --output text)
+aws ec2 attach-internet-gateway --vpc-id $vpc_id --internet-gateway-id $igw_id --region $region
 
-aws ec2 create-route --route-table-id $rt_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id
+rt_id=$(aws ec2 create-route-table --vpc-id $vpc_id --region $region --query RouteTable.RouteTableId --output text)
 
-aws ec2 associate-route-table --subnet-id $subnet1_id --route-table-id $rt_id
+aws ec2 create-route --route-table-id $rt_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id --region $region
+
+aws ec2 associate-route-table --subnet-id $subnet1_id --route-table-id $rt_id --region $region
